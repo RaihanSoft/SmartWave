fa3a4f240a84df0dad6b39ec133d817c69256fe6
Rayhan Baee <aboraihan14@gmail.com>
2024-09-16 16:38:15 +0600

    hero responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,9 @@
 function Header() {
   return (
-    <div className="w-11/12 mx-auto flex flex-col items-center xl:flex-row gap-4 mt-5">
+    <div className="md:container 2xl:px-16 px-2 mx-auto flex flex-col items-center xl:flex-row gap-4 mt-5">
       <div className="carousel w-full xl:w-2/3">
         <div id="item1" className="carousel-item w-full">
-          <img src="./assets\Popup-banner-3995.webp" className="w-full" />
+          <img className="" src="./assets\Popup-banner-3995.webp" />
         </div>
 
         <div id="item2" className="carousel-item w-full">
@@ -23,11 +23,11 @@ function Header() {
 
       <div className="xl:space-y-4 md:flex xl:flex-col gap-4 xl:gap-0 space-y-4 sm:space-y-0">
         
-        <div className="xl:h-[235px] xl:w-[460px]">
+        <div className="xl:h-[202px] 2xl:h-[240px] xl:w-[460px]">
           <img className="h-full w-full" src=".\assets\Top-Banner-(1)-5483.webp" alt="" />
         </div>
 
-        <div className="xl:h-[235px] xl:w-[460px]">
+        <div className="xl:h-[202px] 2xl:h-[240px] xl:w-[460px]">
           <img className="h-full w-full" src=".\assets\Top-Banner-2306.webp" alt="" />
         </div>
 
