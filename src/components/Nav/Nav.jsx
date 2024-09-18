632d91de9146ae2223db9000310fa845080de7a0
Raihan <aboraihan14@gmail.com>
2024-09-18 17:28:36 +0600

    added

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -4,7 +4,7 @@ import './Nav.css'
 function Nav() {
   return (
     <div>
-      <section className="flex items-center justify-between xl:px-10  shadow-lg py-3 px-2">
+      <section className="flex items-center justify-between xl:px-10 md:shadow-lg shadow-lg py-3 px-2">
         {/* logo  */}
         
 
